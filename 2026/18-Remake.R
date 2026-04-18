c37104f74612d91da66d4c0ad1b63e659968dc5b
EvaMaleckova <eva.maleckova@hhu.de>
2026-04-18 21:14:09 +0200

    Added plot caption

--- 2026/18-Remake.R
+++ 2026/18-Remake.R
@@ -17,6 +17,10 @@ source("commons/CreateSocialCaption.R")
 # Get & pre-process data--------------------------------------------------------
 # ---------------------------------------------------------------------------- #
 
+# Original source
+# https://data.metabrainz.org/pub/musicbrainz/data/fullexport/20260418-002325/
+
+
 
 # Plot -------------------------------------------------------------------------
 # ---------------------------------------------------------------------------- #
@@ -25,7 +29,7 @@ font_add_google(name = "Lobster", family = "lobster")
 font_add_google(name = "League Spartan", family = "spartan")
 showtext_auto()
 
-# Background
+# Plot background with caption
 
 circ_sequence <- seq(1, 100, by = 5)
 
@@ -35,16 +39,24 @@ circles <- data.frame(
   y = rep(1, length(circ_sequence))
 )
 
+plot_cap <- paste0(
+  "<span style='color:white;'>**Data source:** musicbrainz database</span>",
+  "<br>",
+  CreateSocialCaption(github_icon_color = "#F2D649", linkedin_icon_color = "#F2D649", text_color = "white")
+  )
+
 p_bg <- ggplot(data = circles, aes(x = 1, y = 1, size = size)) +
-  geom_point(shape = 21, fill = NA, colour = "grey90") +
+  geom_point(shape = 21, fill = NA, colour = "white") +
   scale_size_continuous(range = c(10, 100)) +
   scale_x_continuous(limits = c(-2, 5)) +
   scale_y_continuous(limits = c(-1, 2)) +
-  labs(title = "Most Covered Songs of the King") +
+  labs(title = "Most Covered Songs of the King",
+       caption = plot_cap) +
   theme_void() +
   theme(panel.background = element_rect(fill = "black"),
         plot.background = element_rect(fill = "black"),
-        plot.title = element_text(family = "lobster", size = 22, colour = "white", hjust = 0.5),
+        plot.title = element_text(family = "lobster", size = 30, colour = "white", hjust = 0.5),
+        plot.caption = element_markdown(lineheight = 1.15, hjust = 0.95, align_widths = F),
         legend.position = "none")
 
 # Data
@@ -54,7 +66,7 @@ dat = data.frame(
   covers = c(10, 20, 50)
 )
 
-# colours from paltte https://color.adobe.com/s-Presley-color-theme-1560137/
+# colours from palette https://color.adobe.com/s-Presley-color-theme-1560137/
 
 p_covers <- ggplot(data = dat, aes(x = year, y = covers)) +
   geom_point_blur(colour = "#F2D649", size = 5, blur_size = 10) +
@@ -72,17 +84,16 @@ p_covers <- ggplot(data = dat, aes(x = year, y = covers)) +
   theme_void() +
   theme(plot.background = element_rect(fill = fill_alpha("black", 0.75)),
         plot.title = element_text(family = "lobster", size = 22, colour = "white", hjust = 0.5),
+        aspect.ratio = 0.65,
         legend.position = "none")
 
 p_covers
 
-ggdraw(p_bg) +
-  draw_plot(p_covers, x = -0.05, y = 0.05, scale = 0.5)
+p_assembled <- ggdraw(p_bg) +
+  draw_plot(p_covers, x = -0.05, y = 0.05, scale = 0.65)
 
 # Data as insert
 
+p_assembled
 
-
-print(p)
-
-ggsave(plot = p, filename = "2026/plots/18.png", width = 1200, height = 800, units = "px", bg = "white", scale = 2.5)
+ggsave(plot = p_assembled, filename = "2026/plots/18.png", width = 1200, height = 800, units = "px", bg = "white", scale = 2.5)
