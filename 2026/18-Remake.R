d355d537da7f5c1ad47c146e31552dc203dea95b
EvaMaleckova <eva.maleckova@hhu.de>
2026-04-18 18:32:39 +0200

    Layout draft with dummy data

---
+++ 2026/18-Remake.R
@@ -0,0 +1,88 @@
+# Set-up -----------------------------------------------------------------------
+# ---------------------------------------------------------------------------- #
+
+library(janitor)
+library(dplyr)
+library(tidyr)
+
+library(ggplot2)
+library(ggblur)
+library(ggtext)
+library(showtext)
+library(glue)
+library(cowplot)
+
+source("commons/CreateSocialCaption.R")
+
+# Get & pre-process data--------------------------------------------------------
+# ---------------------------------------------------------------------------- #
+
+
+# Plot -------------------------------------------------------------------------
+# ---------------------------------------------------------------------------- #
+
+font_add_google(name = "Lobster", family = "lobster")
+font_add_google(name = "League Spartan", family = "spartan")
+showtext_auto()
+
+# Background
+
+circ_sequence <- seq(1, 100, by = 5)
+
+circles <- data.frame(
+  size = circ_sequence,
+  x = rep(1, length(circ_sequence)),
+  y = rep(1, length(circ_sequence))
+)
+
+p_bg <- ggplot(data = circles, aes(x = 1, y = 1, size = size)) +
+  geom_point(shape = 21, fill = NA, colour = "grey90") +
+  scale_size_continuous(range = c(10, 100)) +
+  scale_x_continuous(limits = c(-2, 5)) +
+  scale_y_continuous(limits = c(-1, 2)) +
+  labs(title = "Most Covered Songs of the King") +
+  theme_void() +
+  theme(panel.background = element_rect(fill = "black"),
+        plot.background = element_rect(fill = "black"),
+        plot.title = element_text(family = "lobster", size = 22, colour = "white", hjust = 0.5),
+        legend.position = "none")
+
+# Data
+
+dat = data.frame(
+  year = c(1960, 1970, 1980),
+  covers = c(10, 20, 50)
+)
+
+# colours from paltte https://color.adobe.com/s-Presley-color-theme-1560137/
+
+p_covers <- ggplot(data = dat, aes(x = year, y = covers)) +
+  geom_point_blur(colour = "#F2D649", size = 5, blur_size = 10) +
+  geom_point(colour = "#F2C84B", size = 4) +
+  annotate(geom = "text", x = dat[1, ]$year, y = dat[1, ]$covers,
+           label = glue("Covered {dat[1, ]$covers}x"),
+           size = 4, colour = "white", family = "spartan",
+           hjust = -0.5, vjust = -1) +
+  geom_text(data = dat[2:nrow(dat), ],
+            aes(x = year, y = covers, label = paste0(covers, "x")),
+            size = 4, colour = "white", family = "spartan",
+            hjust = -0.5, vjust = -1) +
+  scale_x_continuous(expand = expansion(add = c(10, 10))) +
+  scale_y_continuous(expand = expansion(add = c(5, 5))) +
+  theme_void() +
+  theme(plot.background = element_rect(fill = fill_alpha("black", 0.75)),
+        plot.title = element_text(family = "lobster", size = 22, colour = "white", hjust = 0.5),
+        legend.position = "none")
+
+p_covers
+
+ggdraw(p_bg) +
+  draw_plot(p_covers, x = -0.05, y = 0.05, scale = 0.5)
+
+# Data as insert
+
+
+
+print(p)
+
+ggsave(plot = p, filename = "2026/plots/18.png", width = 1200, height = 800, units = "px", bg = "white", scale = 2.5)
